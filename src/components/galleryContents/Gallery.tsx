b5fbf08db7bf1a7d8e91ff642257f4f987a5936d
hr0614 <ykyk.work@gmail.com>
2025-03-11 00:48:13 +0900

    masonryの仮実装

--- src/components/galleryContents/Gallery.tsx
+++ src/components/galleryContents/Gallery.tsx
@@ -1,18 +1,20 @@
+import { div } from 'framer-motion/client';
 import React, { useState, useEffect } from 'react'
 // import s from '../galleryContents/gallery.module.css'
+import Masonry from 'react-layout-masonry';
 
 
-// type Item = {
-//   image?: {
-//     url: string
-//   },
-//   category: string[],
-//   comment: string[]
-// // }
-// <Item[]>
+type Item = {
+  image?: {
+    url: string
+  },
+  category: string[],
+  comment: string[]
+}
+
 
 export const Gallery = () => {
-  const [items, setItems] = useState()
+  const [items, setItems] = useState<Item[]>()
   console.log(items);
   const fetchItems = async () => {
     const res = await fetch("https://hari-test.microcms.io/api/v1/gallery", {
@@ -29,9 +31,15 @@ export const Gallery = () => {
   }, [])
 
   return (
-    <div className='gallery_wrapper'>
-      hoge
-    </div>
+    <Masonry
+      columns={4}
+      gap={16}
+    >
+      {items && items.map((item, index) => {
+        return <img src={item.image?.url} alt="" key={index} />;
+      })}
+    </Masonry>
   )
 }
+
 
